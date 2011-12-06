7383139c3fddb57c1050ac0957cedc9a072260ec
Josep M. Bach <josep.m.bach@gmail.com>
2011-12-06 18:55:10 +0100

    Refactor tests

--- test/springpad/api_test.rb
+++ test/springpad/api_test.rb
@@ -2,10 +2,10 @@ require 'test_helper'
 
 module Springpad
   describe API do
-    it 'initializes a user and password from ~/.springpad' do
-      config = YAML.load(File.read(File.expand_path("~/.springpad")))
+    let(:config) { YAML.load(File.read(File.expand_path("~/.springpad"))) }
+    let(:api) { API.new }
 
-      api = API.new
+    it 'initializes a user and password from ~/.springpad' do
       api.user.must_equal config['user']
       api.password.must_equal config['password']
       api.token.must_equal config['token']
